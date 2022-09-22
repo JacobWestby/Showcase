a7dde752d1f12d7df41488d7053a14ba5cef3857
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-09-22 15:56:58 +0200

    added button animations and onscroll effect

--- script/script.js
+++ script/script.js
@@ -1,14 +1,37 @@
-const getMiddle = str => {
 
-    if (str.length % 2 === 0) {
+const header = document.querySelector("#header");
+const wave = document.querySelector(".header__bottom-background");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const bigCircle = document.querySelector(".header__cont-inner-circles-outer");
+const smallCircle = document.querySelector(".header__cont-inner-circles-inner");
 
-        return str[(str.length / 2) - 1] + str[str.length / 2];
+const abc = ["a", "b", "c", "d", "e", "f"];
 
-    } else {
-        return str[Math.floor(str.length / 2)];
-    }
+// On click, set new wave background
+let i = 0;
 
-};
+btn1.addEventListener("click", () => {
+    i === 6 ? i = 1 : i++;
 
-getMiddle("hello");
-getMiddle("byee");
+    wave.style.backgroundImage = `url('../images/wave${i}.svg')`;
+});
+
+// On click, set new colors
+
+btn2.addEventListener("click", () => {
+    const h = Math.floor(Math.random() * 8);
+    const e = Math.floor(Math.random() * 8);
+    const c = abc[Math.floor(Math.random() * 5)];
+    const x = abc[Math.floor(Math.random() * 5)];
+
+    header.style.backgroundImage = `linear-gradient(143deg, #${h + e + c + x} 30%, rgba(255,255,255,1) 30%)`;
+    smallCircle.style.fill = `#${h + c + x + c}`;
+});
+
+// Move circle on scroll
+window.addEventListener("scroll", () => {
+    let scrollPos = Math.floor(window.scrollY) / 4;
+    smallCircle.style.transform = `rotate(${scrollPos}deg)`;
+})
